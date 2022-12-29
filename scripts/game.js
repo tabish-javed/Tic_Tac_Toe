dc970fcbf07a384ce5cb488cb9432e88fcf7080b
TABISH <tabishjaved@hotmail.com>
2022-12-29 15:48:33 +0530

    completed game

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+    activePlayer = 0
+    currentRound = 1
+    gameIsOver = false
+    gameOverElement.firstChild.innerHTML =
+        "You won! <span id=\"winner-name\">PLAYER NAME</span>!"
+    gameOverElement.style.display = "none"
+
+    let gameBoardIndex = 0
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+            gameData[i][j] = 0
+            gameBoardElement.children[gameBoardIndex].textContent = ""
+            gameBoardElement.children[gameBoardIndex].classList.remove("disabled")
+            gameBoardIndex++
+        }
+    }
+}
+
 function startNewGame() {
     if (players[0].name === "" || players[1].name === "") {
         alert("Please set custom player names for both players!")
         return
     }
+
+    resetGameStatus()
+
     activePlayerNameElement.textContent = players[activePlayer].name
     gameAreaElement.style.display = "block"
 }
@@ -17,7 +39,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-    if (event.target.tagName !== "LI") {
+    if (event.target.tagName !== "LI" || gameIsOver) {
         return
     }
     const selectedField = event.target
@@ -35,7 +57,11 @@ function selectGameField(event) {
     gameData[selectedRow][selectedColumn] = activePlayer + 1
 
     const winnerId = checkForGameOver()
-    console.log(winnerId)
+
+    if (winnerId !== 0) {
+        endGame(winnerId)
+    }
+
     currentRound++
     switchPlayer()
 }
@@ -77,4 +103,16 @@ function checkForGameOver() {
         return -1
     }
     return 0
+}
+
+function endGame(winnerId) {
+    gameIsOver = true
+    gameOverElement.style.display = "block"
+
+    if (winnerId > 0) {
+        const winnerName = players[winnerId - 1].name
+        gameOverElement.firstElementChild.firstElementChild.textContent = winnerName
+    } else {
+        gameOverElement.firstElementChild.textContent = "It's a draw!"
+    }
 }
